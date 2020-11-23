4b3204f100c4b9907b7495edd4e424a74b2e8751
unknown <adhil>
2020-11-23 07:24:18 -0800

    phone auth page designed

--- app/src/main/java/com/whatsappclone/activitys/PhoneAuthActivity.java
+++ app/src/main/java/com/whatsappclone/activitys/PhoneAuthActivity.java
@@ -2,15 +2,37 @@ package com.whatsappclone.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.hbb20.CountryCodePicker;
 import com.whatsappclone.R;
+import com.whatsappclone.activitys.phoneAuht.PhoneAuthActivitia;
 
 public class PhoneAuthActivity extends AppCompatActivity {
+    CountryCodePicker ccp;
+    TextView mCountryCode,mPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_auth);
+
+        init();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void init() {
+        ccp= findViewById(R.id.ccp);
+        mCountryCode = findViewById(R.id.country_code_tv);
+        mPhoneNumber = findViewById(R.id.phone_tv);
+        mCountryCode.setText(ccp.getSelectedCountryCodeAsInt()+"");
+    }
+
+    public void startPhoneAuthActivity(View view) {
+        startActivity(new Intent(PhoneAuthActivity.this, PhoneAuthActivitia.class));
     }
 }
---
+++ app/src/main/java/com/whatsappclone/activitys/phoneAuht/PhoneAuthActivitia.java
@@ -0,0 +1,16 @@
+package com.whatsappclone.activitys.phoneAuht;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.whatsappclone.R;
+
+public class PhoneAuthActivitia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_auth_activitia);
+    }
+}
